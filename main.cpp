6d8b488e90092ba3258286c4e6dbc52d433c8f34
fadd00 <andhikahutama9@gmail.com>
2024-04-02 15:26:00 +0700

    apalah

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+struct Mahasiswa
+{
+    string nim;
+    string nama;
+    string alamat;
+    string umur;
+};
+int main(){
+    Mahasiswa mhs;
+    cout << "nomor mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "nama mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "alamat mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "umur mahasiswa : ";
+    cin >> mhs.umur;
+    cout << endl;
+    cout << "\n nim : " << mhs.nim;
+    cout << "\n nama : " << mhs.nama;
+    cout << "\n alamat : " << mhs.alamat;
+    cout << "\n umur : " << mhs.umur;
+}
